d7505e95a5c15d03f923c6ef7f2cfd99cabd86ec
vswee <github@sinaswee.com>
2022-10-05 23:40:44 +0100

    intermediate changes

--- pages/homepage/scripts.js
+++ pages/homepage/scripts.js
@@ -55,16 +55,27 @@ document.body.addEventListener("click", minimiseTile);
 
 
 function showcaseYScrollManipulation() {
-  let transition = (window.scrollY - document.querySelector(".showcase").clientTop) / window.outerHeight * .5;
-  document.querySelector(".showcase-slider > input").value = 100 * transition,
-  showcaseTranslateBy = (document.querySelector(".showcase").scrollWidth - document.querySelector(".showcase-parent").getBoundingClientRect().width) * transition,
+  let transition = (window.scrollY - document.querySelector(".showcase").clientTop) / window.outerHeight * .5
+  document.querySelector(".showcase-slider > input").value = 100 * transition
+  let showcaseTranslateBy = (document.querySelector(".showcase").scrollWidth - document.querySelector(".showcase-parent").getBoundingClientRect().width) * transition
   document.querySelector(".showcase").scrollLeft = showcaseTranslateBy
+  let dots = document.querySelector(".slider-ui").querySelectorAll(".dot")
+  let breakPoint = 1 / dots.length
+  let nearest = Math.ceil(transition / breakPoint)
+  let scale = nearest - (transition / breakPoint)
+  console.log(scale)
+  dots.forEach((ele) => ele.style.transform = "scale(1)")
+  dots[nearest].style.transform = "scale(" + (1 + (scale)) + ")"
+  // try { dots[nearest + 1].style.transform = "scale("+ (1+(1-scale)) +")" }catch(e){}
+  // try { dots[nearest - 1].style.transform = "scale("+ (1+(1-scale)) +")" }catch(e){}
+  // try { dots[nearest - 1].style.transform = "scale("+ (1+(scale*.25)) +")" }catch(e){}
+  // try { dots[nearest + 1].style.transform = "scale("+ (1+(scale*.25)) +")" }catch(e){}
 }
 
 function manipulateElements(transition) {
   showcaseTranslateBy = (document.querySelector(".showcase").scrollWidth - document.querySelector(".showcase-parent").getBoundingClientRect().width) * transition,
-  document.querySelector(".showcase").scrollLeft = showcaseTranslateBy,
-  document.querySelector(".slider-ui").querySelector(".blob").style.transform="translateX(calc(" + transition*document.querySelector(".slider-ui").getBoundingClientRect().width + "px - 1rem))"
+  document.querySelector(".showcase").scrollLeft = showcaseTranslateBy
+  // document.querySelector(".slider-ui").querySelector(".blob").style.transform="translateX(calc(" + transition*document.querySelector(".slider-ui").getBoundingClientRect().width + "px - 1rem))"
 }
 
 window.addEventListener("scroll", showcaseYScrollManipulation)
@@ -72,7 +83,7 @@ window.addEventListener("scroll", showcaseYScrollManipulation)
 document.querySelector(".showcase").addEventListener("scroll", e => {
   let t = document.querySelector(".showcase").scrollLeft / (document.querySelector(".showcase").scrollWidth - document.querySelector(".showcase-parent").getBoundingClientRect().width);
   document.querySelector(".showcase-slider > input").value = t * 100
-  document.querySelector(".slider-ui").querySelector(".blob").style.transform="translateX(calc(" + t*document.querySelector(".slider-ui").getBoundingClientRect().width + "px - 1rem))"
+  // document.querySelector(".slider-ui").querySelector(".blob").style.transform="translateX(calc(" + t*document.querySelector(".slider-ui").getBoundingClientRect().width + "px - 1rem))"
 })
 
 document.querySelector(".showcase-slider > input").value = 50, document.querySelector(".showcase-slider > input").addEventListener("input", e => {
