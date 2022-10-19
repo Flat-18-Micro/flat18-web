6ba12652e070ab43fda211501582d9d58c2d789b
vswee <github@sinaswee.com>
2022-10-19 17:15:55 +0100

    hero animation

--- pages/homepage/scripts.js
+++ pages/homepage/scripts.js
@@ -47,3 +47,66 @@ if (document.querySelectorAll(".showcase").length > 0 && document.querySelectorA
       document.querySelector(".slider-ui").append(dot)
   }
 }
+
+
+
+
+
+let TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+  let i = this.loopNum % this.toRotate.length;
+  let fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  let that = this;
+  let delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = fullTxt.length * 700//this.period;
+    console.log(delta)
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 200;
+  }
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+// window.onload = function() {
+  let writers = document.querySelectorAll('.typewriter-magic');
+  for (const write of writers) {
+      let toRotate = write.getAttribute('data-type');
+      let period = write.getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(write, JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  // let css = document.createElement("style");
+  // css.type = "text/css";
+  // css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  // document.body.appendChild(css);
+// };
+
