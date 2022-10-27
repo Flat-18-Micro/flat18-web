56e565c0709c6ad17b9d5e857b196be496643031
vswee <github@sinaswee.com>
2022-10-27 02:11:30 +0100

    minor style improvements

--- pages/homepage/scripts.js
+++ pages/homepage/scripts.js
@@ -1,44 +1,44 @@
-function showcaseYScrollManipulation() {
-  let transition = (window.scrollY - document.querySelector(".showcase").clientTop) / window.outerHeight * .5
-  document.querySelector(".showcase-slider > input").value = 100 * transition
-  let showcaseTranslateBy = (document.querySelector(".showcase").scrollWidth - document.querySelector(".showcase-parent").getBoundingClientRect().width) * transition
-  document.querySelector(".showcase").scrollLeft = showcaseTranslateBy
-  let dots = document.querySelector(".slider-ui").querySelectorAll(".dot")
-  let breakPoint = 1 / dots.length
-  let nearest = Math.ceil(transition / breakPoint)
-  let scale = nearest - (transition / breakPoint)
-  dots.forEach((ele) => ele.style.transform = "scale(1)")
-  try { dots[nearest].style.transform = "scale(" + (1.1 + (scale)) + ")" } catch (e) { }
-  try { dots[nearest - 1].style.transform = "scale(" + (1 + (scale * .5)) + ")" } catch (e) { }
-  try { dots[nearest + 1].style.transform = "scale(" + (1 + (scale * .5)) + ")" } catch (e) { }
-}
-function manipulateElements(transition) {
-  showcaseTranslateBy = (document.querySelector(".showcase").scrollWidth - document.querySelector(".showcase-parent").getBoundingClientRect().width) * transition,
-    document.querySelector(".showcase").scrollLeft = showcaseTranslateBy
-  let dots = document.querySelector(".slider-ui").querySelectorAll(".dot")
-  dots.forEach((ele) => ele.style.transform = "scale(1)")
-}
-window.addEventListener("scroll", showcaseYScrollManipulation)
-document.querySelector(".showcase").addEventListener("scroll", e => {
-  let t = document.querySelector(".showcase").scrollLeft / (document.querySelector(".showcase").scrollWidth - document.querySelector(".showcase-parent").getBoundingClientRect().width);
-  document.querySelector(".showcase-slider > input").value = t * 100
-})
-document.querySelector(".showcase-slider > input").value = 50
-document.querySelector(".showcase-slider > input").addEventListener("input", e => {
-  if ("INPUT" === e.target.nodeName) {
-    window.removeEventListener("scroll", showcaseYScrollManipulation);
-    let t = Number(e.target.value) / 100;
-    manipulateElements(t)
-  }
-})
-if (document.querySelectorAll(".showcase").length > 0 && document.querySelectorAll(".slider-ui").length > 0) {
-  let items = document.querySelector(".showcase").querySelectorAll(".item")
-  for (i = 0; i < items.length; i++) {
-    let dot = document.createElement("span")
-    dot.classList.add("dot")
-    document.querySelector(".slider-ui").append(dot)
-  }
-}
+// function showcaseYScrollManipulation() {
+//   let transition = (window.scrollY - document.querySelector(".showcase").clientTop) / window.outerHeight * .5
+//   document.querySelector(".showcase-slider > input").value = 100 * transition
+//   let showcaseTranslateBy = (document.querySelector(".showcase").scrollWidth - document.querySelector(".showcase-parent").getBoundingClientRect().width) * transition
+//   document.querySelector(".showcase").scrollLeft = showcaseTranslateBy
+//   let dots = document.querySelector(".slider-ui").querySelectorAll(".dot")
+//   let breakPoint = 1 / dots.length
+//   let nearest = Math.ceil(transition / breakPoint)
+//   let scale = nearest - (transition / breakPoint)
+//   dots.forEach((ele) => ele.style.transform = "scale(1)")
+//   try { dots[nearest].style.transform = "scale(" + (1.1 + (scale)) + ")" } catch (e) { }
+//   try { dots[nearest - 1].style.transform = "scale(" + (1 + (scale * .5)) + ")" } catch (e) { }
+//   try { dots[nearest + 1].style.transform = "scale(" + (1 + (scale * .5)) + ")" } catch (e) { }
+// }
+// function manipulateElements(transition) {
+//   showcaseTranslateBy = (document.querySelector(".showcase").scrollWidth - document.querySelector(".showcase-parent").getBoundingClientRect().width) * transition,
+//     document.querySelector(".showcase").scrollLeft = showcaseTranslateBy
+//   let dots = document.querySelector(".slider-ui").querySelectorAll(".dot")
+//   dots.forEach((ele) => ele.style.transform = "scale(1)")
+// }
+// window.addEventListener("scroll", showcaseYScrollManipulation)
+// document.querySelector(".showcase").addEventListener("scroll", e => {
+//   let t = document.querySelector(".showcase").scrollLeft / (document.querySelector(".showcase").scrollWidth - document.querySelector(".showcase-parent").getBoundingClientRect().width);
+//   document.querySelector(".showcase-slider > input").value = t * 100
+// })
+// document.querySelector(".showcase-slider > input").value = 50
+// document.querySelector(".showcase-slider > input").addEventListener("input", e => {
+//   if ("INPUT" === e.target.nodeName) {
+//     window.removeEventListener("scroll", showcaseYScrollManipulation);
+//     let t = Number(e.target.value) / 100;
+//     manipulateElements(t)
+//   }
+// })
+// if (document.querySelectorAll(".showcase").length > 0 && document.querySelectorAll(".slider-ui").length > 0) {
+//   let items = document.querySelector(".showcase").querySelectorAll(".item")
+//   for (i = 0; i < items.length; i++) {
+//     let dot = document.createElement("span")
+//     dot.classList.add("dot")
+//     document.querySelector(".slider-ui").append(dot)
+//   }
+// }
 let TxtType = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
